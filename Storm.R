bf1701ec653b998bfb36d26f09cf252e0c67445d
DhavalBisure <dhavalbisure98@gmail.com>
2020-07-03 11:05:09 +0530

    Created plots

--- Storm.R
+++ Storm.R
@@ -9,12 +9,34 @@ storm <- read.csv(bzfile('StormData.csv.bz2'),header=TRUE, stringsAsFactors = FA
 ##Summarising the data and checking for NA values in needed columns
 summary(storm)
 
-##Finding harmful events
-harmData <- aggregate(storm$FATALITIES, by = list(storm$EVTYPE), FUN = sum)
-harmData2 <- aggregate(storm$INJURIES, by = list(storm$EVTYPE), FUN = sum)
-harm <- merge(harmData,harmData2, by.x = "Group.1", by.y = "Group.1")
-names(harm) <- c("Event","Fatalities","Injuries")
-harm <- harm[order(-harm$Injuries),]
-head(harm)
+##Finding events with highest Fatality
+FatalityData <- aggregate(storm$FATALITIES, by = list(storm$EVTYPE), FUN = sum)
+names(FatalityData) <- c("Event","Fatalities")
+FatalityData <- FatalityData[order(-FatalityData$Fatalities),]
+FatalityData <- FatalityData[1:20,]
+head(FatalityData)
 
-##Creating plot
+
+##Finding events with highest Injuries
+InjuryData <- aggregate(storm$INJURIES, by = list(storm$EVTYPE), FUN = sum)
+names(InjuryData) <- c("Event","Injuries")
+InjuryData <- InjuryData[order(-InjuryData$Injuries),]
+InjuryData <- InjuryData[1:20,]
+head(InjuryData)
+
+##Creating plot for harmful events
+library(ggplot2)
+
+fatalplot <- ggplot(FatalityData,aes(y=Fatalities,x= reorder(Event, -Fatalities))) +
+ geom_bar(stat="identity",fill="steelblue") +
+ theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
+ ggtitle("Events with Highest Fatality") + xlab("Events") + ylab("Number of Fatalities")
+
+injuryplot <- ggplot(InjuryData,aes(y=Injuries,x= reorder(Event, -Injuries))) +
+  geom_bar(stat="identity",fill="red") +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
+  ggtitle("Events with Highest Injuries") + xlab("Events") + ylab("Number of Injuries")
+
+library(gridExtra)
+grid.arrange(fatalplot,injuryplot,ncol=2)
+ 
